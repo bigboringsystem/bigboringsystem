a45710bf8dcbaa404014ada0f4e23180ccdf4b43
Zach Bruggeman <mail@bruggie.com>
2014-12-28 12:15:34 -0800

    Show unread chat count in tab when chat page not active

--- public/js/global.js
+++ public/js/global.js
@@ -4,7 +4,6 @@
   var socket = io();
   var count = 0;
   var getChatSessionStorage = window.sessionStorage.getItem('chat');
-  var unreadMessages = (window.sessionStorage.getItem('unread') || 0);
   var chatArr = [];
 
   if (getChatSessionStorage){
@@ -20,22 +19,8 @@
     window.sessionStorage.setItem('chat', JSON.stringify(chatArr));
   }
 
-  if (window.location.pathname === '/chat') {
-    unreadMessages = 0;
-    window.sessionStorage.setItem('unread', 0);
-  }
-  if (unreadMessages > 0) {
-    document.querySelector('.chat-link').innerText = 'chat (' + unreadMessages + ')';
-  }
-
   socket.on('message', function (data) {
     chatArr.push(data);
     window.sessionStorage.setItem('chat', JSON.stringify(chatArr));
-
-    if (window.location.pathname !== '/chat') {
-      unreadMessages++;
-      window.sessionStorage.setItem('unread', unreadMessages);
-      document.querySelector('.chat-link').innerText = 'chat (' + unreadMessages + ')';
-    }
   });
 })();
